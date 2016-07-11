a6d7014f40cd5d7eaffc59c831acf2bcd03c091f
Decima <lostop@126.com>
2016-07-11 19:18:16 +0800

    基础版本 最终修订
    最终修订

---
+++ HighChartLocalExport/Export.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.UI;
+using HighChartLocalExport.ExportHttpHandler;
+
+namespace HighChartLocalExport
+{
+    public partial class Export : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ExportChart.ProcessExportRequest(Context);
+            }
+        }
+    }
+}
